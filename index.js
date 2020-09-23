9623c5325290f7a85cd439a490b5d317b238974c
Leho Kraav <leho@kraav.com>
2020-09-23 16:46:14 +0300

    refactor: `composer exec` Windows compatibility

--- index.js
+++ index.js
@@ -10,12 +10,21 @@ module.exports = {
             .map((file) => path.relative(cwd, file))
             .join(' ');
 
+        const phpComposerExec = 'composer exec -v';
         const phpConfigs = {
             'phpcs.xml': [
-                `./vendor/bin/phpcbf ${relativeFilenames}; if [ $? -eq 1 ]; then exit 0; fi`,
-                `./vendor/bin/phpcs -s ${relativeFilenames}`,
+                /**
+                 * Until PHPCS v4.
+                 * Requires updated https://github.com/conversionxl/phpcs-ruleset
+                 *
+                 * @see https://github.com/squizlabs/PHP_CodeSniffer/issues/2898
+                 * @see https://github.com/composer/composer/issues/9244
+                 */
+                // `${phpComposerExec} phpcbf ${relativeFilenames}; if [ $? -eq 1 ]; then exit 0; fi`,
+                `${phpComposerExec} phpcbf-fix-exit-0 -- ${relativeFilenames}`,
+                `${phpComposerExec} phpcs -- -s ${relativeFilenames}`,
             ],
-            'psalm.xml': [`./vendor/bin/psalm ${relativeFilenames}`],
+            'psalm.xml': [`${phpComposerExec} psalm -- ${relativeFilenames}`],
         };
 
         // @see https://reedbarger.com/how-to-transform-javascript-objects-the-power-of-objectkeys-values-entries/#replacing-map--filter-with-a-single-reduce
